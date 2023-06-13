57e41648e4f81adcb28a0ef9e18c5485580c8a75
melisamirando <melisamirando@gmail.com>
2023-06-13 14:34:32 +0800

    modified js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-  for (tablinks of tablinks) {
-    tablinks.classList.remove("active-link");
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
   }
-  for (tabcontents of tabcontents) {
-    tabcontents.classList.remove("active-tab");
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
